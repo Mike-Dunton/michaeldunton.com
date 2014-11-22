355df905f28c06bd1d445e5c33497f43802676aa
Michael Dunton <Michael.Dunton@patlive.com>
2014-11-22 00:11:05 -0500

    Moving navigation to its own file and adding navigation styles

--- index.php
+++ index.php
@@ -16,38 +16,14 @@
             <link rel="icon" href="/favicon.ico" type="image/x-icon">
             <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     </head>
-    <body>
+    <body id="home">
         <div class="container-fluid">
         <div class="header">
         <p>~/portfolio/index.php</p>
         </div>
             <div class="height-maximizer">
                     <div class="row">
-                        <div class="col-md-2 column-one">
-                            <img class="center-block" src="http://www.gravatar.com/avatar/44c419ced8cb577d774287a9e5929223?s=286" />
-                            <div class="sidebar-navigation-container">
-                                <div class="nav-item-first-level">
-                                    <i class="fa fa-github"></i>
-                                    <a href="https://github.com/Mike-Dunton">FollowMeOn.github</a>
-                                </div>
-                                <div class="nav-item-first-level">
-                                    <i class="fa fa-twitter-square"></i>
-                                    <a href="https://twitter.com/Mike_Dunton">FollowMeOn.twitter</a>
-                                </div>
-                                <div class="nav-item-first-level">
-                                    <i class="fa fa-folder-open-o"></i>
-                                    <span>michaeldunton.com</span>
-                                </div>
-                                <div class="nav-item-second-level">
-                                    <i class="fa fa-file-text-o"></i>
-                                    <a href="#">readme.md</a>
-                                </div>
-                                <div class="nav-item-second-level">
-                                    <i class="fa fa-github"></i>
-                                    <a href="#">source</a>
-                                </div>
-                            </div>
-                        </div>
+                    <?php include_once("navigation.html"); ?>
                         <div class="col-md-10 column-two">
                             <p>Michael Dunton was here.</p>
                         </div>
